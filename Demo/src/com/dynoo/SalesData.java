53cbe78be369c17f067166c494cdfea8c7ca564b
dinesh <dinesh@Dinesh-PC.fios-router.home>
2017-01-18 19:36:57 -0500

    DisplayNum - commit

--- Demo/src/com/dynoo/SalesData.java
+++ Demo/src/com/dynoo/SalesData.java
@@ -5,5 +5,9 @@ public class SalesData {
 	public void displayDetails(){
 		
 		System.out.println("TEST SALES DISPLAY DATA");
+		
+		for(int i =1;i<5;i++){
+			System.out.println("Value : "+i);
+		}
 	}
 }
