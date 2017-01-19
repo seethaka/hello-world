cf81556ef7143577430674092f07c0ba9718385f
dinesh <dinesh@Dinesh-PC.fios-router.home>
2017-01-18 19:29:01 -0500

    Sales - Commit

---
+++ Demo/src/com/dynoo/SalesData.java
@@ -0,0 +1,9 @@
+package com.dynoo;
+
+public class SalesData {
+
+	public void displayDetails(){
+		
+		System.out.println("TEST SALES DISPLAY DATA");
+	}
+}
---
+++ Demo/src/com/dynoo/SalesMain.java
@@ -0,0 +1,13 @@
+package com.dynoo;
+
+public class SalesMain {
+
+	public static void main(String[] args) {
+
+		SalesData data = new SalesData();
+		
+		System.out.println("Sales Main demo");
+		data.displayDetails();
+	}
+
+}
