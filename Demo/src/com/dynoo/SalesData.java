25848b19261eb0d10f24a91ef1ab788a43ae42f2
dinesh <dinesh@Dinesh-PC.fios-router.home>
2017-01-18 19:46:30 -0500

    Numbers

--- Demo/src/com/dynoo/SalesData.java
+++ Demo/src/com/dynoo/SalesData.java
@@ -2,8 +2,13 @@ package com.dynoo;
 
 public class SalesData {
 
+	private int data[]={-1,0,15,24};
 	public void displayDetails(){
 		
 		System.out.println("TEST SALES DISPLAY DATA");
+		
+		for(int i:data){
+			System.out.println("value :"+data[i]);
+		}
 	}
 }
